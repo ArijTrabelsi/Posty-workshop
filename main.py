18e89db59ba23631bc21f869be294a0ba53a61a0
Arij Trabelsi <52330025+ArijTrabelsi@users.noreply.github.com>
2019-07-23 11:33:22 -0700

    add main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from storee import Post, PostStore
+from flask import Flask,render_template,request,redirect,url_for
+from store import Post, PostStore
 
 app = Flask(__name__)
 
@@ -11,35 +11,39 @@ dummy_posts = [
     Post(id=2,
          photo_url='https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=100&w=100',
          name='John',
-         body='Lorem Ipsum'),
+         body='Lorem Ipsum')
 ]
+
 post_store = PostStore()
 post_store.add(dummy_posts[0])
 post_store.add(dummy_posts[1])
+app.current_id = 3
 
 @app.route('/')
+@app.route('/index')
 def home():
+    return render_template('index.html',posts=post_store.get_all())
 
-    return render_template('index.html', posts=post_store.get_all())
-
-@app.route('/post/add', methods=['GET','POST'])
+@app.route('/post/add',methods=['GET', 'POST'])
 def post_add():
-    if request.method == 'POST':
-        print('done!')
-        new_post = Post(id=0,photo_url=request.form['photo_url'],name=request.form['name'],body=request.form['body'])
+    if request.method=='POST':
+        new_post = Post(id=app.current_id, photo_url=request.form['photo_url'],
+                        name=request.form['name'],
+                        body=request.form['body'])
         post_store.add(new_post)
+        app.current_id += 1
+        return redirect(url_for('home'))
+    elif request.method=='GET':
+        return render_template('post-add.html')
+
+
+@app.route('/posts/delete/<int:id>')
+def post_delete(id):
+        post_store.delete(id)
         return redirect(url_for('home'))
 
-    elif request.method == 'GET':
-        return render_template('add_post.html')
-
-@app.route('/post/delete/<int:id>')
-def post_delete(id):
-    post_store.delete(id)
-    return redirect(url_for('home'))
-
-@app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
-def post_edit(id):
+@app.route('/posts/update/<int:id>', methods = ['GET', 'POST'])
+def post_update(id):
     if request.method == 'POST':
         update_fields = {
             'photo_url': request.form['photo_url'],
@@ -51,5 +55,5 @@ def post_edit(id):
         return redirect(url_for('home'))
     elif request.method == 'GET':
         post = post_store.get_by_id(id)
-        return render_template('edit_post.html', post=post)
-
+        print('hi')
+        return render_template('post-update.html', post=post)

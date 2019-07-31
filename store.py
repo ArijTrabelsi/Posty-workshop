e7362ac00adcc80671d31eecfcc913b6e2270153
Arij Trabelsi <52330025+ArijTrabelsi@users.noreply.github.com>
2019-07-31 13:20:35 -0700

    add store.py

--- store.py
+++ store.py
@@ -1,7 +1,7 @@
 class Post:
 
     def __init__(self, id, photo_url, name, body):
-        
+
         self.id = id
         self.photo_url = photo_url
         self.name = name
@@ -12,36 +12,35 @@ posts = []
 class PostStore:
 
     def get_all(self):
-         # get all posts - الحصول على كل المنشورات
-        for post in posts :
-            return post
+        # get all posts - الحصول على كل المنشورات
+        return posts
 
     def add(self, post):
         # append post - إضافة منشور
-        self.post = post
         posts.append(post)
         return posts
 
     def get_by_id(self, id):
-        # search for post by id -id البحث عن منشور بالمعرف 
+        # search for post by id -id البحث عن منشور بالمعرف
         result = None
         self.id = id
         for post in posts :
             if post.id == id :
                 result = post
                 break
+
         return result
 
     def update(self, id, fields):
-        # update post data - id تعديل منشور بالمعرف 
+        # update post data - id تعديل منشور بالمعرف
         post = self.get_by_id(id)
-        post.photo_url = fields['photo_url']
         post.name = fields['name']
+        post.photo_url = fields['photo_url']
         post.body = fields['body']
         return post
-       
+
     def delete(self, id):
-        # delete post by id - id حذف المنشور بالمعرف   
+        # delete post by id - id حذف المنشور بالمعرف
         post = self.get_by_id(id)
         posts.remove(post)
         return posts
